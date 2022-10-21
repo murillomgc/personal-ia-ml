4486499117c3a895c034b5a382446fbbe281677f
Murillo A. Fermino <murillo.fermino@gmail.com>
2022-10-21 00:18:10 -0300

    Updating files

---
+++ learning-basics/02-plotly-dash/02-dash-basics/dash_example.py
@@ -0,0 +1,27 @@
+import dash
+from dash import dcc
+from dash import html
+
+
+app = dash.Dash()
+
+app.layout = html.Div(
+    children=[
+        html.H1("Hello Dash!"),
+        html.Div("Dash: A web application framework for Python."),
+        dcc.Graph(
+            id="example-graph",
+            figure={
+                "data": [
+                    {"x": [1, 2, 3], "y": [4, 1, 2], "type": "bar", "name": "SF"},
+                    {"x": [1, 2, 3], "y": [2, 4, 5], "type": "bar", "name": "NYC"},
+                ],
+                "layout": {"title": "Dash Data Visualization"},
+            },
+        ),
+    ]
+)
+
+
+if __name__ == "__main__":
+    app.run_server()
